b114355b5a126b78c9e19920afa01075a32ee39e
Troy Benson <troybensonsa@gmail.com>
2026-03-09 14:20:05 +0200

    try again

--- setup-nix/scripts/nix-upload-cache.sh
+++ setup-nix/scripts/nix-upload-cache.sh
@@ -7,8 +7,13 @@ if [[ ! -f "${NIX_CACHE_PRIVATE_KEY_FILE:-}" ]]; then
 	exit 1
 fi
 
-if [[ -z "${NIX_CACHE_URL:-}" ]]; then
-	echo "Error: NIX_CACHE_URL not set"
+if [[ -z "${S3_CACHE_BUCKET:-}" ]]; then
+	echo "Error: S3_CACHE_BUCKET not set"
+	exit 1
+fi
+
+if [[ -z "${S3_CACHE_ENDPOINT:-}" ]]; then
+	echo "Error: S3_CACHE_ENDPOINT not set"
 	exit 1
 fi
 
@@ -16,7 +21,7 @@ if [[ -s /tmp/nix-built-paths.txt ]]; then
 	echo "Uploading $(wc -l </tmp/nix-built-paths.txt) paths to cache..."
 	mapfile -t paths < <(sort -u /tmp/nix-built-paths.txt)
 	nix copy \
-		--to "s3://${S3_CACHE_BUCKET}/${S3_CACHE_NIX_PREFIX}?endpoint=${S3_CACHE_ENDPOINT}&scheme=https&compression=zstd&parallel-compression=true&secret-key=${NIX_CACHE_PRIVATE_KEY_FILE}&multipart-upload=true" \
+		--to "s3://${S3_CACHE_BUCKET}/${S3_CACHE_NIX_PREFIX:-}?endpoint=${S3_CACHE_ENDPOINT}&scheme=https&compression=zstd&parallel-compression=true&secret-key=${NIX_CACHE_PRIVATE_KEY_FILE}&multipart-upload=true" \
 		"${paths[@]}" ||
 		echo "Warning: nix copy failed, continuing anyway"
 else
